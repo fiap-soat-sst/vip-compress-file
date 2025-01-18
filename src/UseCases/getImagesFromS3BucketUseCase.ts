58a68074840683091cb3be64f8176f109a93a14d
evilfeeh <felipegoncalvesdsantos@gmail.com>
2025-01-17 21:00:59 -0300

    feat: create usecases

---
+++ src/UseCases/getImagesFromS3BucketUseCase.ts
@@ -0,0 +1,1 @@
+export async function getImagesFromS3BucketUseCase(imageMetadata: unknown) {}
---
+++ src/UseCases/uploadImagesToS3BucketUseCase.ts
@@ -0,0 +1,3 @@
+export async function uploadImagesToS3BucketUseCase(
+  compressedImages: unknown
+) {}
