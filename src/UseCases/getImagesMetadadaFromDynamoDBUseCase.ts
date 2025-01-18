e0b20ef4ab33c1a915e2ee73b47f4a0ed60b8c0f
evilfeeh <felipegoncalvesdsantos@gmail.com>
2025-01-17 21:45:26 -0300

    feat: get bucketName of imageId

--- src/UseCases/getImagesMetadadaFromDynamoDBUseCase.ts
+++ src/UseCases/getImagesMetadadaFromDynamoDBUseCase.ts
@@ -1,1 +1,6 @@
-export async function getImagesMetadadaFromDynamoDBUseCase(message: unknown) {}
+import { DatabaseRepository } from '../Gateways/ImageRepository'
+
+export async function getBucketNameFromDynamoDBUseCase(messageId: string) {
+  const databaseRepository = new DatabaseRepository()
+  return databaseRepository.getBucket(messageId)
+}
