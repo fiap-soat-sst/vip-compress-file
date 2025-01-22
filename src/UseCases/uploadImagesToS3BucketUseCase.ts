d3597a101ce7e47b8cdc5a2d8dcbb87f1da309a1
evilfeeh <felipegoncalvesdsantos@gmail.com>
2025-01-22 20:12:50 -0300

    feat: defining upload class

--- src/UseCases/uploadImagesToS3BucketUseCase.ts
+++ src/UseCases/uploadImagesToS3BucketUseCase.ts
@@ -1,3 +1,12 @@
+import { IBucketStorageGateway } from '../Gateways/IBucketStorageGateway'
+
 export class UploadImagesToS3BucketUseCase {
-  async execute(compressedFolder: unknown) {}
+  constructor(private readonly imageRepository: IBucketStorageGateway) {}
+  async execute(compressedFolder: Buffer) {
+    return this.imageRepository.upload(
+      compressedFolder,
+      'compressed',
+      'application/zip'
+    )
+  }
 }
